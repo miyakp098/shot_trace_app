99343ebe5007101b45228968095098a06d377bdb
miyakp098 <ybtkeppen813@gmail.com>
2025-11-25 16:13:40 +0900

    ホーム画面表示コードの移動

---
+++ lib/screens/home/home_viewmodel.dart
@@ -0,0 +1,8 @@
+// Simple placeholder for Home screen view model.
+// Extend or replace with ChangeNotifier / Riverpod / Bloc as needed.
+
+class HomeViewModel {
+  int counter = 0;
+
+  void increment() => counter++;
+}
